5abc72248ec33df85ea95104880d80f07bb06641
vulgoct <catarinatravaglia@icloud.com>
2025-04-11 20:53:04 -0300

    Contagem de 1 a 10
    	new file:   Exercicio01.java
    	new file:   ../../ExerciciosJavaLista1/.idea/vcs.xml
    	new file:   ../../ExerciciosJavaLista1/.idea/workspace.xml

---
+++ ExercicioJavaList1/src/Exercicio01.java
@@ -0,0 +1,9 @@
+public class Exercicio01 {
+    public static void main(String[] args) {
+        int numero = 10;
+
+        for (int i = 1; i <= numero; i++){
+            System.out.println(i);
+        }
+    }
+}
