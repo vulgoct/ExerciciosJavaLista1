fe8aec51618e4732f309f8eb27671d3f7ee8362a
vulgoct <catarinatravaglia@icloud.com>
2025-04-13 22:10:51 -0300

    Tabuada de um número

---
+++ ExercicioJavaList1/src/Exercicio03.java
@@ -0,0 +1,12 @@
+public class Exercicio03 {
+    public static void main(String[]args){
+        int N = 5;
+
+        for(int i = 1; i <= 10; i++){
+            int mult = N * i;
+            System.out.println("5 X " + i + " = " + mult);
+        }
+        
+    }
+    
+}
