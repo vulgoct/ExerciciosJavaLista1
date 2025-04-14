91842e7a1af1b621f1d254a1e48b6315d2012296
vulgoct <catarinatravaglia@icloud.com>
2025-04-13 22:11:47 -0300

    Números pares até N

---
+++ ExercicioJavaList1/src/Exercicio04.java
@@ -0,0 +1,12 @@
+public class Exercicio04 {
+    public static void main(String[]args){
+        int N = 10;
+
+        for(int i = 1; i <= N; i++){
+            if(i % 2 == 0){
+                System.out.println(i + " é Par");
+            }
+        }
+    }
+    
+}
