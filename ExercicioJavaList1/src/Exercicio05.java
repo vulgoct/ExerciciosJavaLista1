2e6b407a831e6cbf84abefa2aa5dbaceb1886276
vulgoct <catarinatravaglia@icloud.com>
2025-04-13 22:12:29 -0300

    Números ímpares até N

---
+++ ExercicioJavaList1/src/Exercicio05.java
@@ -0,0 +1,11 @@
+public class Exercicio05 {
+    public static void main(String[]args){
+        int N = 1;
+        while(N <= 10){
+            if(N % 2 != 0){
+                System.out.println(N + " é Impar");         
+            } 
+            N++;
+        }
+    }
+ }
