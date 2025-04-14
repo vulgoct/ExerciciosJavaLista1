0f2836f28d8a3a783938fc9c5c87944937668fba
vulgoct <catarinatravaglia@icloud.com>
2025-04-13 22:13:07 -0300

    Fatorial de um número

---
+++ ExercicioJavaList1/src/Exercicio06.java
@@ -0,0 +1,10 @@
+public class Exercicio06 {
+    public static void main(String[]args){
+        int N = 5;
+
+        for(int F = 0; F != N; F++){
+            int results = N % F;
+            System.out.println(results);
+        }
+    }
+}
