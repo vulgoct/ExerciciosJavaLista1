26d76f44b3bb3205f3f7f42e0a7d006eba4f826a
vulgoct <catarinatravaglia@icloud.com>
2025-04-13 22:03:47 -0300

    Soma de 1 a N

--- ExercicioJavaList1/src/Exercicio02.java
+++ ExercicioJavaList1/src/Exercicio02.java
@@ -1,3 +1,12 @@
 public class Exercicio02 {
-    public static
+    public static void main(String[]args){
+        int N = 5;
+        int soma = 0;
+
+        for(int i = 1; i <= N; i++){
+            soma += i;
+        }
+        System.out.println("1+2+3+4+5 = " + soma);
+    }
+    
 }
